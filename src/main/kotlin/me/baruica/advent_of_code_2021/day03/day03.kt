ad774b20a04cb77304e6a2c2d3b4a6e67294d173
Nelson da Costa <baruica@gmail.com>
2021-12-11 00:15:48 +0100

    wip

--- src/main/kotlin/me/baruica/advent_of_code_2021/day03/day03.kt
+++ src/main/kotlin/me/baruica/advent_of_code_2021/day03/day03.kt
@@ -5,15 +5,13 @@ import java.io.File
 val diagnosticReport = File("inputs/day03.txt").readLines()
 
 fun main() {
-    println(part1())
+    part1()
+    part2()
 }
 
-private fun part1(): Int {
+private fun part1() {
     val counts = counts()
-    val gammaRate = counts.fold("") { gRate, count -> if (isMostCommon(count)) gRate+"1" else gRate+"0" }
-    val epsilonRate = counts.fold("") { eRate, count -> if (isLeastCommon(count)) eRate+"1" else eRate+"0" }
-
-    return gammaRate.toInt(2) * epsilonRate.toInt(2)
+    println(gammaRate(counts).toInt(2) * epsilonRate(counts).toInt(2))
 }
 
 private fun counts(): Array<Int> {
@@ -26,6 +24,44 @@ private fun counts(): Array<Int> {
     return values
 }
 
-private fun isMostCommon(count: Int): Boolean = count > (diagnosticReport.size / 2)
+private fun gammaRate(counts: Array<Int>) = counts.fold("") { gRate, count -> if (count > (diagnosticReport.size / 2)) gRate+"1" else gRate+"0" }
 
-private fun isLeastCommon(count: Int): Boolean = count < (diagnosticReport.size / 2)
+private fun epsilonRate(counts: Array<Int>) = counts.fold("") { eRate, count -> if (count < (diagnosticReport.size / 2)) eRate+"1" else eRate+"0" }
+
+private fun part2() {
+    println(oxygenGeneratorRating(diagnosticReport).toInt(2) * co2ScrubberRating(diagnosticReport).toInt(2))
+}
+
+private fun oxygenGeneratorRating(numbers: List<String>): String {
+    var oxygenGeneratorRating = ""
+    var filteredNumbers: List<String>
+
+    for (currentBitPosition in numbers.first().indices) {
+        val bitCriteria = if (numbers.sumOf { it[currentBitPosition].digitToInt() } >= (numbers.size / 2)) 1 else 0
+        filteredNumbers = numbers.filter { it[currentBitPosition].digitToInt() == bitCriteria }
+
+        if (filteredNumbers.size == 1) {
+            oxygenGeneratorRating = filteredNumbers.first()
+            break
+        }
+    }
+
+    return oxygenGeneratorRating
+}
+
+private fun co2ScrubberRating(numbers: List<String>): String {
+    var co2ScrubberRating = ""
+    var filteredNumbers: List<String>
+
+    for (currentBitPosition in numbers.first().indices) {
+        val bitCriteria = if (numbers.sumOf { it[currentBitPosition].digitToInt() } <= (numbers.size / 2)) 0 else 1
+        filteredNumbers = numbers.filter { it[currentBitPosition].digitToInt() == bitCriteria }
+
+        if (filteredNumbers.size == 1) {
+            co2ScrubberRating = filteredNumbers.first()
+            break
+        }
+    }
+
+    return co2ScrubberRating
+}
