fae5b045623a0870f0a1d9fbaef20049586234a0
Nelson <baruica@gmail.com>
2024-04-08 18:55:36 +0200

    wip(day03)

---
+++ src/main/kotlin/me/baruica/aoc2023/Day03.kt
@@ -0,0 +1,17 @@
+package me.baruica.aoc2023
+
+import java.io.File
+
+val engineSchematic = File("inputs/2023/Day03.txt").readLines()
+
+fun main() {
+    part1()
+}
+
+private fun part1() {
+    println(
+        // add up all the part numbers (any number adjacent to a symbol, . does not count)
+    )
+}
+
+val symbols = listOf("*", "#", "+", "$", "@", "=", "%", "&")
---
+++ src/test/kotlin/me/baruica/aoc2023/Day03Test.kt
@@ -0,0 +1,22 @@
+package me.baruica.aoc2023
+
+import io.kotest.core.spec.style.StringSpec
+
+class Day03Test : StringSpec({
+    "" {
+        val input = """
+            |467..114..
+            |...*......
+            |..35..633.
+            |......#...
+            |617*......
+            |.....+.58.
+            |..592.....
+            |......755.
+            |...$.*....
+            |.664.598..
+        """.trimMargin()
+
+        // 4361
+    }
+})
